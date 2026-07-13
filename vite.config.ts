4621e5cdf4108b2763ca01e037e73fdfd31c521b
Praddume <praddumeattri1@gmail.com>
2026-07-13 23:12:07 +0530

    Fix Vercel build timeout issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "force-exit-after-build",
+      apply: "build",
+      closeBundle() {
+        // @ts-ignore
+        console.log("Build finished, forcing exit...");
+        // @ts-ignore
+        setTimeout(() => process.exit(0), 100);
+      },
+    },
+  ],
 });
